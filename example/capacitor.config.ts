d1a61668a9064a42c388bd178a975731f592c5e7
jcesarmobile <jcesarmobile@gmail.com>
2024-05-17 18:01:34 +0200

    chore(example): Update app to Capacitor 6 (#14)

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'Ad Id Example',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  webDir: 'dist'
 };
 
 export default config;
